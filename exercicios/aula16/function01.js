6dc3f598d2b3495749fe0343fbc5076a4cc90727
Bruno Calsoni <114168109+brunocalsoni@users.noreply.github.com>
2022-11-18 19:17:56 -0300

    Create function01.js

---
+++ exercicios/aula16/function01.js
@@ -0,0 +1,16 @@
+function parimp(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+/*
+
+let res = parimp(155)
+console.log(res) 
+
+*/
+
+console.log(parimp(132))
